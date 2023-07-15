bdb7859bc74c98f3480433bc13c9dca29f60f1d4
yasudis <67751529+yasudis@users.noreply.github.com>
2023-07-15 10:45:30 +0700

    add Range project

--- Range/src/ru/academits/yasudis/range_main/Main.java
+++ Range/src/ru/academits/yasudis/range_main/Main.java
@@ -1,7 +1,30 @@
 package ru.academits.yasudis.range_main;
 
+import ru.academits.yasudis.range.Range;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] arg){
-        System.out.println("hello world");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Range range = new Range(10, 25.5);
+
+        System.out.println("Диапазон начинается: от " + range.getFrom() + " до " + range.getTo());
+        System.out.println("Длина диапазона равна: " + range.getLength());
+
+        range.setFrom(30.5);
+        range.setTo(80.5);
+
+        System.out.println("Диапазон начинается: от " + range.getFrom() + " до " + range.getTo());
+        System.out.println("Длина диапазона равна: " + range.getLength());
+
+        System.out.println("Введите число:");
+        double number = scanner.nextDouble();
+
+        if (range.isRange(number)) {
+            System.out.println("Число " + number + " в диапазоне.");
+        } else {
+            System.out.println("Число " + number + " не в диапазоне");
+        }
     }
 }
