8aa697f16c5a6cb0379debcbf490cb10c2a94dcf
Yasudis Oleg <67751529+yasudis@users.noreply.github.com>
2023-09-02 12:15:23 +0700

    Добавление новых методов и отработка их в маине

--- Range/src/ru/academits/yasudis/range/Range.java
+++ Range/src/ru/academits/yasudis/range/Range.java
@@ -7,6 +7,8 @@ public class Range {
     public Range(double from, double to) {
         this.from = from;
         this.to = to;
+
+        sortRange();
     }
 
     public double getFrom() {
@@ -31,5 +33,76 @@ public class Range {
 
     public boolean isRange(double number) {
         return number >= from && number <= to;
+    }
+
+    private void sortRange() {
+        if (to < from) {
+            double temp = to;
+            to = from;
+            from = temp;
+        }
+    }
+
+    private boolean isIntersection(Range rangeB) {
+        return this.from <= rangeB.to && this.to >= rangeB.from;
+    }
+
+    public Double[] getIntersectionTwoIntervals(Range rangeB) {
+        if (!(isIntersection(rangeB))) {
+            return null;
+        }
+
+        Double[] result = new Double[2];
+
+        result[0] = Math.max(this.from, rangeB.from);
+        result[1] = Math.min(this.to, rangeB.to);
+
+        return result;
+    }
+
+    public Range[] combiningIntervals(Range rangeB) {
+        Range[] result = new Range[2];
+
+        if (!isIntersection(rangeB)) {
+            result[0] = new Range(this.from, this.to);
+            result[1] = rangeB;
+
+            return result;
+        }
+
+        double from = Math.min(this.from, rangeB.from);
+        double to = Math.max(this.to, rangeB.to);
+
+        result[0] = new Range(from, to);
+        return result;
+    }
+
+    public Range[] subtractingIntervals(Range rangeB) {
+        Range[] result = new Range[2];
+
+        if (!isIntersection(rangeB)) {
+            result[0] = new Range(from, to);
+            return result;
+        }
+
+        if (rangeB.from > this.from) {
+            result[0] = new Range(this.from, rangeB.from);
+
+        } else {
+            result[0] = null;
+
+        }
+        if (rangeB.to > this.to) {
+            result[1] = null;
+        } else {
+            result[1] = new Range(rangeB.to, this.to);
+        }
+
+        return result;
+    }
+
+
+    public String showIntervals() {
+        return (" от " + getFrom() + " до " + getTo());
     }
 }
--- Range/src/ru/academits/yasudis/range_main/Main.java
+++ Range/src/ru/academits/yasudis/range_main/Main.java
@@ -1,30 +1,68 @@
 package ru.academits.yasudis.range_main;
 
 import ru.academits.yasudis.range.Range;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Range range = new Range(10, 25.5);
+        Range rangeA = new Range(10, 25.5);
+        Range rangeB = new Range(50, 20);
 
-        System.out.println("Диапазон начинается: от " + range.getFrom() + " до " + range.getTo());
-        System.out.println("Длина диапазона равна: " + range.getLength());
+        System.out.println("Диапазон начинается: от " + rangeA.getFrom() + " до " + rangeA.getTo());
+        System.out.println("Длина диапазона равна: " + rangeA.getLength());
 
-        range.setFrom(30.5);
-        range.setTo(80.5);
+        rangeA.setFrom(30.5);
+        rangeA.setTo(80.5);
 
-        System.out.println("Диапазон начинается: от " + range.getFrom() + " до " + range.getTo());
-        System.out.println("Длина диапазона равна: " + range.getLength());
+        System.out.println("Диапазон начинается: от " + rangeA.getFrom() + " до " + rangeA.getTo());
+        System.out.println("Длина диапазона равна: " + rangeA.getLength());
 
         System.out.println("Введите число:");
         double number = scanner.nextDouble();
 
-        if (range.isRange(number)) {
+        if (rangeA.isRange(number)) {
             System.out.println("Число " + number + " в диапазоне.");
         } else {
             System.out.println("Число " + number + " не в диапазоне");
         }
+
+        Double[] intersectionTwoIntervals = rangeA.getIntersectionTwoIntervals(rangeB);
+
+        if (intersectionTwoIntervals != null) {
+            System.out.println("Пересечение двух диапазонов равно: " + Arrays.toString(intersectionTwoIntervals));
+        } else {
+            System.out.println("Пересечения нет.");
+        }
+
+        Range[] combinedIntervals = rangeA.combiningIntervals(rangeB);
+
+        if (combinedIntervals[1] == null) {
+            System.out.println("Объединения двух диапазонов равно: " + combinedIntervals[0].showIntervals());
+        } else {
+            System.out.println("Объединения двух диапазонов равно: " + combinedIntervals[0].showIntervals() + " и " +
+                    combinedIntervals[1].showIntervals());
+        }
+
+        Range[] subtractingIntervals = rangeA.subtractingIntervals(rangeB);
+
+        if (subtractingIntervals[0] == null) {
+            if (subtractingIntervals[1] == null) {
+                System.out.println("Разность двух диапазонов равно нулю.");
+            } else {
+                System.out.println("Разность двух диапазонов равно: " + subtractingIntervals[1].showIntervals());
+            }
+        } else {
+            if (subtractingIntervals[1] == null) {
+                System.out.println("Разность двух диапазонов равно: " + subtractingIntervals[0].showIntervals());
+            } else {
+                System.out.println("Разность двух диапазонов равно: " + subtractingIntervals[0].showIntervals() + "и" +
+                        subtractingIntervals[1].showIntervals());
+            }
+        }
+
     }
 }
