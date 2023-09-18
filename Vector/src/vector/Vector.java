b8806c5b999e8e270f9571fd5652c53810800913
Yasudis Oleg <67751529+yasudis@users.noreply.github.com>
2023-09-18 23:15:40 +0700

    доделал задачу, на проверку

--- Vector/src/vector/Vector.java
+++ Vector/src/vector/Vector.java
@@ -33,6 +33,32 @@ public class Vector {
         coordinates = Arrays.copyOf(array, n);
     }
 
+    public static Vector getSumVectors(Vector vector1, Vector vector2) {
+        Vector resultVector = new Vector(vector1);
+        resultVector.sumVectors(vector2);
+
+        return resultVector;
+    }
+
+    public static double getScalarMultiply(Vector vector1, Vector vector2) {
+        double resultMultiply = 0;
+
+        int minVectorLength = Math.min(vector1.coordinates.length, vector2.coordinates.length);
+
+        for (int i = 0; i < minVectorLength; i++) {
+            resultMultiply += vector1.coordinates[i] * vector2.coordinates[i];
+        }
+
+        return resultMultiply;
+    }
+
+    public static Vector getDifferenceVectors(Vector vector1, Vector vector2) {
+        Vector resultVector = new Vector(vector1);
+        resultVector.differenceVectors(vector2);
+
+        return resultVector;
+    }
+
     public int getSize() {
         return this.coordinates.length;
     }
@@ -54,5 +80,90 @@ public class Vector {
         sb.append("}");
 
         return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (object == null || this.getClass() != object.getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) object;
+
+        if (this.coordinates.length != vector.coordinates.length) {
+            return false;
+        }
+
+        for (int i = 0; i < this.coordinates.length; i++) {
+            if (this.coordinates[i] != vector.coordinates[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(this.coordinates);
+    }
+
+    public void sumVectors(Vector vector) {
+        if (this.coordinates.length < vector.coordinates.length) {
+            this.coordinates = Arrays.copyOf(this.coordinates, vector.coordinates.length);
+        }
+
+        for (int i = 0; i < vector.coordinates.length; i++) {
+            this.coordinates[i] += vector.coordinates[i];
+        }
+    }
+
+    public void differenceVectors(Vector vector) {
+        if (this.coordinates.length < vector.coordinates.length) {
+            this.coordinates = Arrays.copyOf(this.coordinates, vector.coordinates.length);
+        }
+
+        for (int i = 0; i < vector.coordinates.length; i++) {
+            this.coordinates[i] -= vector.coordinates[i];
+        }
+    }
+
+    public void multiplyVectorOnNumber(double number) {
+        for (int i = 0; i < this.coordinates.length; i++) {
+            this.coordinates[i] *= number;
+        }
+    }
+
+    public void rotateVector() {
+        this.multiplyVectorOnNumber(-1);
+    }
+
+    public double getVectorLength() {
+        double lengthVector = 0;
+
+        for (double coordinate : this.coordinates) {
+            lengthVector += Math.pow(coordinate, 2);
+        }
+
+        return Math.sqrt(lengthVector);
+    }
+
+    public double getCoordinateByIndex(int index) {
+        if (index < 0 || index >= this.coordinates.length) {
+            throw new ArrayIndexOutOfBoundsException("неверный индекс");
+        }
+        return this.coordinates[index];
+    }
+
+    public void setCoordinateByIndex(int index, double value) {
+        if (index < 0 || index >= this.coordinates.length) {
+            throw new ArrayIndexOutOfBoundsException("неверный индекс");
+        }
+
+        this.coordinates[index] = value;
     }
 }
--- Vector/src/vector_main/Main.java
+++ Vector/src/vector_main/Main.java
@@ -4,12 +4,54 @@ import vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
+        double[] array1 = {34.3, 22.2, 1.0, 1.25};
 
-        System.out.println("Hello world!");
-        double[] array = {1, 1, 1, 1};
+        Vector vector1 = new Vector(array1);
+        System.out.println("vector1 " + vector1);
 
-        Vector vector1 = new Vector(array);
+        System.out.println("размерность vector1 равно " + vector1.getSize());
 
-        System.out.print(vector1);
+        Vector vector2 = new Vector(5);
+        System.out.println("vector2 " + vector2);
+
+        Vector vector3 = new Vector(vector1);
+        System.out.println("vector3 " + vector3);
+
+        Vector vector4 = new Vector(6, array1);
+        System.out.println("vector4 " + vector4);
+
+        vector1.sumVectors(vector4);
+        System.out.println("сумма vector1 и vector4: " + vector1);
+
+        vector4.differenceVectors(vector1);
+        System.out.println("разность vector4 и vector1: " + vector4);
+
+        vector4.multiplyVectorOnNumber(4);
+        System.out.println("скалярное произведение vector4: " + vector4);
+
+        vector4.rotateVector();
+        System.out.println("инверсия вектора vector4: " + vector4);
+
+        System.out.println("Длина вектора vector1: " + vector1.getVectorLength());
+
+        vector1.setCoordinateByIndex(1, 12);
+        System.out.println("задание значения  в индексе 1 в vector1 на 12: " + vector1);
+
+        System.out.println("vector4 имеет координату под индексом 1 = " + vector4.getCoordinateByIndex(1));
+
+        Vector vector5 = Vector.getSumVectors(vector3, vector4);
+        System.out.println("получение vector5 суммированием : " + vector5);
+
+        double[] array2 = {2, 2, 2, 2};
+        double[] array3 = {1, 2, 3, 4, 0, 0, 0};
+
+        Vector vector6 = new Vector(array2);
+        Vector vector7 = new Vector(array3);
+
+        Vector vector8 = Vector.getDifferenceVectors(vector6, vector7);
+        System.out.println("получение vector8 вычитанием: " + vector8);
+
+        double scalarMultiply = Vector.getScalarMultiply(vector6, vector7);
+        System.out.println("скалярное произведение vector6 и vector7 равно: " + scalarMultiply);
     }
 }
